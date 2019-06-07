3648bbbc50d96b68f01db92b61d7293471dc009f
yudinmaksym <4ygakk@gmail.com>
2019-06-07 15:26:53 +0300

    Add files via upload

---
+++ build/precache-manifest.398c3caa9ca03fa2c2c8fb5c0b3e05ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3438a57ddb2273fe26676340e9a105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7da772279affd7c53ec",
+    "url": "/static/css/main.4575ae16.chunk.css"
+  },
+  {
+    "revision": "8385aa1554e906db55df",
+    "url": "/static/js/2.73226ec3.chunk.js"
+  },
+  {
+    "revision": "a7da772279affd7c53ec",
+    "url": "/static/js/main.9a93dcc5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
